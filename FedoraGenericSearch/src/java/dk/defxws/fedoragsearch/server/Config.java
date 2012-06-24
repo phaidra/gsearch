0149606af874ee8229f393691c783fa9499529cb
Rastislav Hudak <hudak.rastislav@gmail.com>
2012-06-24 22:15:05 +0200

    customization

--- FedoraGenericSearch/src/java/dk/defxws/fedoragsearch/server/Config.java
+++ FedoraGenericSearch/src/java/dk/defxws/fedoragsearch/server/Config.java
@@ -892,7 +892,7 @@ public class Config {
         return repositoryName;
     }
     
-    private Properties getRepositoryProps(String repositoryName) {
+    public Properties getRepositoryProps(String repositoryName) {
         return (repositoryNameToProps.get(repositoryName));
     }
     
